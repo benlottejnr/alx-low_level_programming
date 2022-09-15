d84a94d5838e09edeed7c0b75480642ec5d1593d
Ben Mensah <benlottejnr@gmail.com>
2022-09-15 16:46:16 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-int main(){
+
+/*
+ * function main prints specific output
+ *
+ * return success on 0
+*/
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return(0);
 }
