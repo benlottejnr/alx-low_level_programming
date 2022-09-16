269bd5d891d3012e29e3fe1402d36f424be986e1
Ben Mensah <benlottejnr@gmail.com>
2022-09-16 08:15:05 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point - checks if a number is positive or negative
+ *
+ * Return: always 0 (Success)
+*/
+int main(void)
+{
+	int ch;
+	
+	ch = tolower(ch)
+	putchar(ch);
+	putchar('\n')
+	return (0);
+}
