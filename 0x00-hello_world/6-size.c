49438ce8748ef9e4a52e8a80b6924c8ff3cf9819
Ben Mensah <benlottejnr@gmail.com>
2022-09-16 03:52:22 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,6 @@ int main(void)
 	int longlongintType;
 	float floatType;
 	char charType;
-
 	printf("Size of a char: %lu byte(s)\n", sizeof(charType));
 	printf("Size of an int: %lu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(longintType));
