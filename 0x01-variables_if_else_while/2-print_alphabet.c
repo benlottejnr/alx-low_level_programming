216a212c81a464575432f2eb997b8e928361c2b2
Ben Mensah <benlottejnr@gmail.com>
2022-09-16 08:19:47 -0700

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry point - checks if a number is positive or negative
+ * main - Entry point
  *
  * Return: always 0 (Success)
 */
@@ -9,7 +9,7 @@ int main(void)
 {
 	int ch;
 	
-	for (ch= ‘a’; ch<= ‘z’; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	putchar(ch);
 	putchar('\n');
 	return (0);
