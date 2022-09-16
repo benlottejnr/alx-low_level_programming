fff56c4ef965840d759bce63e2c31c79f480ebcf
Ben Mensah <benlottejnr@gmail.com>
2022-09-16 09:20:16 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+	char ch;
+	for (n = 0; n <= 16; n++)
+	{
+		ch = ("%x",n)
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
