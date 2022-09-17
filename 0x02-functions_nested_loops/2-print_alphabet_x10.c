0b18bc26d0364c32cfdee8a6ecf2bc173fab121c
Ben Mensah <benlottejnr@gmail.com>
2022-09-17 07:37:57 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,10 @@ void print_alphabet_x10(void)
 	int i;
 
 	for (i = 0; i < 10; i++)
-	for (alpha = 'a'; alpha <= 'z'; alpha++)
-	_putchar(alpha);
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+
 	_putchar('\n');
+	}
 }
