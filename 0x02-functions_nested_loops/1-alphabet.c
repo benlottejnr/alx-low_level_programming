c514101c4e719c64989055e84c2a0d2d42876c9c
Ben Mensah <benlottejnr@gmail.com>
2022-09-17 06:49:45 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+*/
+void print_alphabet(void)
+{
+	int alpha;
+	for (int alpha = 'a'; alpha <= 'z'; alpha++)
+	_putchar(alpha)
+	_putchar('\n');
+}
