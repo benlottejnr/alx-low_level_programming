1d068dda1ae3c5a6cb152a533ae6ca78f5f62827
Ben Mensah <benlottejnr@gmail.com>
2022-09-19 13:50:05 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - Entry point
+ * @n: the reset value
+ *
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98
+
+	return (n);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Directory: 0x05-pointers_arrays_strings
